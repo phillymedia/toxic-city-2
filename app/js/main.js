d7378ec2e657202b3b974fc3d238bf1fb93acdd3
jared-whalen <jwhalen@philly.com>
2017-06-15 19:54:10 -0400

    Video intro

--- app/js/main.js
+++ app/js/main.js
@@ -77,4 +77,32 @@ $(document).ready(function() {
             }
         })
     });
+
+//jared's js
+
+// Get media - with autoplay disabled (audio or video)
+var media = $('#video-intro').not("[autoplay='autoplay']");
+var tolerancePixel = 40;
+
+function checkMedia(){
+    // Get current browser top and bottom
+    var scrollTop = $(window).scrollTop() + tolerancePixel;
+    var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+    media.each(function(index, el) {
+        var yTopMedia = $(this).offset().top;
+        var yBottomMedia = $(this).height() + yTopMedia;
+
+        if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){ //view explaination in `In brief` section above
+            $(this).get(0).play();
+        } else {
+            $(this).get(0).pause();
+        }
+    });
+
+    //}
+}
+$(document).on('scroll', checkMedia);
+
+
 });
